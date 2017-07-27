78b89307b4d63f0d07e2a14296be41f2a5f51f7f
shir19-meet <shir19@meet.mit.edu>
2017-07-27 14:00:08 +0300

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,38 @@
+##c=0
+##for number in range(1,100+1):
+##    print(number)
+##    c=c+number
+##print(c)
+##
+##def add_numbers():
+##    c=0
+##    for add_numbers in range(1,100+1):
+##        print(add_numbers)
+##        c=c+add_numbers
+##        
+##    return(c)
+##answer=add_numbers()
+##print(answer)
+
+
+def add_numbers(start,end):
+    c=0
+    for numbers in range(start,end+1):
+        print(numbers)
+        c=c+numbers
+    return(c)
+answer=add_numbers(333,777)
+print(answer)
+    
+    
+    
+    
+    
+
+
+
+
+    
+    
+    
+    
