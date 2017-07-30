e6e0cc590574c0a2f38cba3498e8aa356e22e016
shir19-meet <shir19@meet.mit.edu>
2017-07-30 13:51:18 +0300

    Stopping here

--- fun2.py
+++ fun2.py
@@ -1,2 +1,20 @@
-print("*****")
+##def draw_1d(n,char):
+##    c1= "~"
+##    c2= "!"
+##    print(n * char)
+
+def draw_1d(n,char):
+     print(n* char)
+
+def draw_2d(n,m,char):
+    for num in range(n):
+        draw_1d(m,char)
+
+
+
+def special_draw_2d(n,m,border,fill)
+
+
+    
+
 
